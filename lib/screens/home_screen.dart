5429071aee124fc25ac51d6f2eeba59dbe855720
SyedSamad7 <syedsamadshah15@gmail.com>
2024-11-11 23:48:24 +0500

    complete

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -61,7 +61,7 @@ class HomeScreen extends StatelessWidget {
                     const SizedBox(
                       height: 20,
                     ),
-                    InkWell(
+                    GestureDetector(
                       onTap: () => Navigator.push(
                           context,
                           MaterialPageRoute(
